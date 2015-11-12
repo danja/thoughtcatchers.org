abf9bbf5c499468ad3fe16821da851bfb2fbe6a4
danja <danny.ayers@gmail.com>
2015-11-12 16:30:14 +0100

    tweak

--- www/js/lib/jQuery-File-Upload-files/main.js
+++ www/js/lib/jQuery-File-Upload-files/main.js
@@ -18,7 +18,8 @@ $(function () {
     $('#fileupload').fileupload({
         // Uncomment the following to send cross-domain cookies:
         //xhrFields: {withCredentials: true},
-        url: 'http://localhost:8082/'
+      //  url: 'http://localhost:8082/'
+      url: 'http://hyperdata.it:8082/'
     });
 
     // Enable iframe cross-domain access via redirect option:
